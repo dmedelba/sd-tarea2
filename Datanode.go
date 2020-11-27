4742aa4358c417df884f0060e3605401bafede02
legorretas <macarena.hidalgo.14@sansano.usm.cl>
2020-11-27 17:45:08 -0300

    arreglado warning de datanode

--- Datanode.go
+++ Datanode.go
@@ -9,10 +9,10 @@ import (
 	"google.golang.org/grpc"
 )
 
-type Server struct {
+type server struct {
 }
 
-func (s *Server) SubirLibro(ctx context.Context, request *uploader.Solicitud_SubirLibro) (*uploader.Respuesta_SubirLibro, error) {
+func (s *server) SubirLibro(ctx context.Context, request *uploader.Solicitud_SubirLibro) (*uploader.Respuesta_SubirLibro, error) {
 	log.Printf("recibi la wea")
 	log.Printf(request.NombreLibro)
 	return &uploader.Respuesta_SubirLibro{Respuesta: int32(0)}, nil
@@ -24,9 +24,8 @@ func main() {
 	if err != nil {
 		log.Fatalf("Error al tratar de escuchar: %v", err)
 	}
-	s := Server{}
-	grpcServer := grpc.NewServer()
-	uploader.RegisterUploaderServer(grpcServer, &s)
+	s := grpc.NewServer()
+	uploader.RegisterUploaderServer(s, &server{})
 	if err := grpcServer.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %s", err)
 	}
