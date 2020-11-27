f31da483315d330a7bbd4e80588746525d141768
David Medel <dmedel.ba@gmail.com>
2020-11-27 00:18:38 -0300

    a

--- Cliente.go
+++ Cliente.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	"log"
 	"fmt"
 	"io/ioutil"
+	"log"
+	"strconv"
 	//"os"
 	//"path/filepath"
 	//"google.golang.org/grpc"
@@ -12,6 +13,7 @@ import (
 const (
 	puerto = "dist70:6000"
 )
+
 /*
 func subirLibroCentralizado(conn *grpc.ClientConn){
 	//buscamos libro, se selecciona y se descompone
@@ -19,67 +21,71 @@ func subirLibroCentralizado(conn *grpc.ClientConn){
 }
 
 func subirLibroDistribuido(conn *grpc.ClientConn){
-	//buscamos libro, se selecciona y se descompone	
+	//buscamos libro, se selecciona y se descompone
 }
 */
-func mostrar_libros(){
+func mostrarLibros() {
 	//presentamos al usuario los libros para que seleccione
 	path := "./libros/"
 	lst, err := ioutil.ReadDir(path)
 	if err != nil {
 		panic(err)
 	}
+	i := 0
 	for _, val := range lst {
 		if val.IsDir() {
 			fmt.Printf("[%s]\n", val.Name())
 		} else {
-			fmt.Println("Numero"+val.Name())
+			s := strconv.Itoa(i)
+			fmt.Println(s + ".- " + val.Name())
+			i++
 		}
 	}
 }
+
 //Establecemos conexión con logisitica dist70:6000
 
-	/*
+/*
 	var conn *grpc.ClientConn
 	conn, e	rr := grpc.Dial(puerto, grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("No se pudo establecer la conexión. ERROR: %v", err)
 	}
-	
+
 	defer conn.Close()
-	*/
-func main(){
+*/
+func main() {
 	//crear conexion
 	var seleccion int
 	flag := true
-	for flag{
+	for flag {
 		log.Printf("[Cliente] Ingrese opición a realizar:")
 		log.Printf("1. Subir libro")
 		log.Printf("2. Descargar libro")
 		log.Printf("3. Ver biblioteca (libros descargados)")
 		log.Printf("4. Finalizar")
 		fmt.Scanln(&seleccion)
-		
-		var tipo_subida int
+
+		var tipoSubida int
 		switch seleccion {
 		case 1:
 			//subir libro, puede subir centralizado o distribuido
 			log.Printf("[Cliente] Seleccione el tipo de subida a realizar:")
 			log.Printf("1. Subida Centralizada")
 			log.Printf("2. Subida Distribuida")
-			fmt.Scanln(&tipo_subida)
-			switch tipo_subida {
+			fmt.Scanln(&tipoSubida)
+			switch tipoSubida {
 			case 1:
 				//subirLibroCentralizado(conn)
 				//centralizada
 			case 2:
-				//distribuida	
+				//distribuida
 				//subirLibroDistribuido(conn)
 			}
 		case 2:
 			//descargar libro, conectarse al name node (69)
 		case 3:
-			mostrar_libros()
+			mostrarLibros()
 			//ver biblioteca
 		case 4:
 			//finalizar
