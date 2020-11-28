acb1274fd354872695900e38ff08e8588507f69e
David Medel <dmedel.ba@gmail.com>
2020-11-27 22:31:02 -0300

    a

--- Datanode.go
+++ Datanode.go
@@ -19,7 +19,7 @@ func (s *server) SubirLibro(ctx context.Context, request *uploader.Solicitud_Sub
 
 	//creo la carpeta para guardar chunks del libro
 	idChunk := strconv.Itoa(request.Id)
-	fileName = "./libros_subidos/" + request.NombreLibro[0:15] + "_" + idChunk
+	fileName := "./libros_subidos/" + request.NombreLibro[0:15] + "_" + idChunk
 	file, errr := os.Create(fileName)
 
 	log.Printf(request.NombreLibro)
