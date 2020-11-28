8ce4f650dd6ba16fea6834fe476109ab309f7e88
David Medel <dmedel.ba@gmail.com>
2020-11-27 22:14:18 -0300

    a

--- Datanode.go
+++ Datanode.go
@@ -19,7 +19,7 @@ func (s *server) SubirLibro(ctx context.Context, request *uploader.Solicitud_Sub
 	//creo la carpeta para guardar chunks del libro
 
 	if _, err := os.Stat(request.NombreLibro); os.IsNotExist(err) {
-		err = os.Mkdir("./chunks/"+request.NombreLibro, 0755)
+		err = os.Mkdir("./librossubidos/"+request.NombreLibro[0:10], 0755)
 		if err != nil {
 			panic(err)
 		}
