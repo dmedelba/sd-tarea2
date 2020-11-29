d3dccf11a07d020e3afe0fa8bce785fede990d36
David Medel <dmedel.ba@gmail.com>
2020-11-29 02:36:34 -0300

    ok

--- Datanode.go
+++ Datanode.go
@@ -62,7 +62,7 @@ func enviarPropuesta(propuesta string, tipoExclusion string) {
 	if tipoExclusion == "1" {
 		//es centralizada, preguntar al name node
 		var conn *grpc.ClientConn
-		conn, err := grpc.Dial("dist69:6002", grpc.WithInsecure())
+		conn, err := grpc.Dial("dist69:6000", grpc.WithInsecure())
 		if err != nil {
 			log.Fatalf("Error al conectarse con la maquina 69 [Name node]. %s", err)
 		}
