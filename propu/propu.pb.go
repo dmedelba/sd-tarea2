39248efbf1cfb1c259263638595cb828f0ccfb72
root <root@dist69.inf.santiago.usm.cl>
2020-12-02 22:03:02 -0300

    a

--- propu/propu.pb.go
+++ propu/propu.pb.go
@@ -131,6 +131,189 @@ func (x *Respuesta_Propuesta) GetRespuesta() string {
 	return ""
 }
 
+//Solicitamos ver los libros almacenados en el NameNode
+type Solicitud_VerLibros struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+}
+
+func (x *Solicitud_VerLibros) Reset() {
+	*x = Solicitud_VerLibros{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_propu_proto_msgTypes[2]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *Solicitud_VerLibros) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*Solicitud_VerLibros) ProtoMessage() {}
+
+func (x *Solicitud_VerLibros) ProtoReflect() protoreflect.Message {
+	mi := &file_propu_proto_msgTypes[2]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use Solicitud_VerLibros.ProtoReflect.Descriptor instead.
+func (*Solicitud_VerLibros) Descriptor() ([]byte, []int) {
+	return file_propu_proto_rawDescGZIP(), []int{2}
+}
+
+//respuesta de los libros
+type Respuesta_VerLibros struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	LibrosDisponibles string `protobuf:"bytes,1,opt,name=librosDisponibles,proto3" json:"librosDisponibles,omitempty"`
+}
+
+func (x *Respuesta_VerLibros) Reset() {
+	*x = Respuesta_VerLibros{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_propu_proto_msgTypes[3]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *Respuesta_VerLibros) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*Respuesta_VerLibros) ProtoMessage() {}
+
+func (x *Respuesta_VerLibros) ProtoReflect() protoreflect.Message {
+	mi := &file_propu_proto_msgTypes[3]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use Respuesta_VerLibros.ProtoReflect.Descriptor instead.
+func (*Respuesta_VerLibros) Descriptor() ([]byte, []int) {
+	return file_propu_proto_rawDescGZIP(), []int{3}
+}
+
+func (x *Respuesta_VerLibros) GetLibrosDisponibles() string {
+	if x != nil {
+		return x.LibrosDisponibles
+	}
+	return ""
+}
+
+//se solicita la ubicacion chunks de los libros
+type Solicitud_Ubicaciones struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	NombreLibro string `protobuf:"bytes,1,opt,name=nombreLibro,proto3" json:"nombreLibro,omitempty"`
+}
+
+func (x *Solicitud_Ubicaciones) Reset() {
+	*x = Solicitud_Ubicaciones{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_propu_proto_msgTypes[4]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *Solicitud_Ubicaciones) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*Solicitud_Ubicaciones) ProtoMessage() {}
+
+func (x *Solicitud_Ubicaciones) ProtoReflect() protoreflect.Message {
+	mi := &file_propu_proto_msgTypes[4]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use Solicitud_Ubicaciones.ProtoReflect.Descriptor instead.
+func (*Solicitud_Ubicaciones) Descriptor() ([]byte, []int) {
+	return file_propu_proto_rawDescGZIP(), []int{4}
+}
+
+func (x *Solicitud_Ubicaciones) GetNombreLibro() string {
+	if x != nil {
+		return x.NombreLibro
+	}
+	return ""
+}
+
+//respuesta de la ubicacion por parte del NameNode
+type Respuesta_Ubicaciones struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Ubicaciones string `protobuf:"bytes,1,opt,name=ubicaciones,proto3" json:"ubicaciones,omitempty"`
+}
+
+func (x *Respuesta_Ubicaciones) Reset() {
+	*x = Respuesta_Ubicaciones{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_propu_proto_msgTypes[5]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *Respuesta_Ubicaciones) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*Respuesta_Ubicaciones) ProtoMessage() {}
+
+func (x *Respuesta_Ubicaciones) ProtoReflect() protoreflect.Message {
+	mi := &file_propu_proto_msgTypes[5]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use Respuesta_Ubicaciones.ProtoReflect.Descriptor instead.
+func (*Respuesta_Ubicaciones) Descriptor() ([]byte, []int) {
+	return file_propu_proto_rawDescGZIP(), []int{5}
+}
+
+func (x *Respuesta_Ubicaciones) GetUbicaciones() string {
+	if x != nil {
+		return x.Ubicaciones
+	}
+	return ""
+}
+
 var File_propu_proto protoreflect.FileDescriptor
 
 var file_propu_proto_rawDesc = []byte{
@@ -144,13 +327,35 @@ var file_propu_proto_rawDesc = []byte{
 	0x69, 0x62, 0x72, 0x6f, 0x22, 0x33, 0x0a, 0x13, 0x52, 0x65, 0x73, 0x70, 0x75, 0x65, 0x73, 0x74,
 	0x61, 0x5f, 0x50, 0x72, 0x6f, 0x70, 0x75, 0x65, 0x73, 0x74, 0x61, 0x12, 0x1c, 0x0a, 0x09, 0x72,
 	0x65, 0x73, 0x70, 0x75, 0x65, 0x73, 0x74, 0x61, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x09,
-	0x72, 0x65, 0x73, 0x70, 0x75, 0x65, 0x73, 0x74, 0x61, 0x32, 0x53, 0x0a, 0x05, 0x50, 0x72, 0x6f,
-	0x70, 0x75, 0x12, 0x4a, 0x0a, 0x0f, 0x45, 0x6e, 0x76, 0x69, 0x61, 0x72, 0x50, 0x72, 0x6f, 0x70,
-	0x75, 0x65, 0x73, 0x74, 0x61, 0x12, 0x19, 0x2e, 0x70, 0x72, 0x6f, 0x70, 0x75, 0x2e, 0x50, 0x72,
-	0x6f, 0x70, 0x75, 0x65, 0x73, 0x74, 0x61, 0x5f, 0x47, 0x65, 0x6e, 0x65, 0x72, 0x61, 0x64, 0x61,
-	0x1a, 0x1a, 0x2e, 0x70, 0x72, 0x6f, 0x70, 0x75, 0x2e, 0x52, 0x65, 0x73, 0x70, 0x75, 0x65, 0x73,
-	0x74, 0x61, 0x5f, 0x50, 0x72, 0x6f, 0x70, 0x75, 0x65, 0x73, 0x74, 0x61, 0x22, 0x00, 0x62, 0x06,
-	0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x72, 0x65, 0x73, 0x70, 0x75, 0x65, 0x73, 0x74, 0x61, 0x22, 0x15, 0x0a, 0x13, 0x53, 0x6f, 0x6c,
+	0x69, 0x63, 0x69, 0x74, 0x75, 0x64, 0x5f, 0x56, 0x65, 0x72, 0x4c, 0x69, 0x62, 0x72, 0x6f, 0x73,
+	0x22, 0x43, 0x0a, 0x13, 0x52, 0x65, 0x73, 0x70, 0x75, 0x65, 0x73, 0x74, 0x61, 0x5f, 0x56, 0x65,
+	0x72, 0x4c, 0x69, 0x62, 0x72, 0x6f, 0x73, 0x12, 0x2c, 0x0a, 0x11, 0x6c, 0x69, 0x62, 0x72, 0x6f,
+	0x73, 0x44, 0x69, 0x73, 0x70, 0x6f, 0x6e, 0x69, 0x62, 0x6c, 0x65, 0x73, 0x18, 0x01, 0x20, 0x01,
+	0x28, 0x09, 0x52, 0x11, 0x6c, 0x69, 0x62, 0x72, 0x6f, 0x73, 0x44, 0x69, 0x73, 0x70, 0x6f, 0x6e,
+	0x69, 0x62, 0x6c, 0x65, 0x73, 0x22, 0x39, 0x0a, 0x15, 0x53, 0x6f, 0x6c, 0x69, 0x63, 0x69, 0x74,
+	0x75, 0x64, 0x5f, 0x55, 0x62, 0x69, 0x63, 0x61, 0x63, 0x69, 0x6f, 0x6e, 0x65, 0x73, 0x12, 0x20,
+	0x0a, 0x0b, 0x6e, 0x6f, 0x6d, 0x62, 0x72, 0x65, 0x4c, 0x69, 0x62, 0x72, 0x6f, 0x18, 0x01, 0x20,
+	0x01, 0x28, 0x09, 0x52, 0x0b, 0x6e, 0x6f, 0x6d, 0x62, 0x72, 0x65, 0x4c, 0x69, 0x62, 0x72, 0x6f,
+	0x22, 0x39, 0x0a, 0x15, 0x52, 0x65, 0x73, 0x70, 0x75, 0x65, 0x73, 0x74, 0x61, 0x5f, 0x55, 0x62,
+	0x69, 0x63, 0x61, 0x63, 0x69, 0x6f, 0x6e, 0x65, 0x73, 0x12, 0x20, 0x0a, 0x0b, 0x75, 0x62, 0x69,
+	0x63, 0x61, 0x63, 0x69, 0x6f, 0x6e, 0x65, 0x73, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x0b,
+	0x75, 0x62, 0x69, 0x63, 0x61, 0x63, 0x69, 0x6f, 0x6e, 0x65, 0x73, 0x32, 0xea, 0x01, 0x0a, 0x05,
+	0x50, 0x72, 0x6f, 0x70, 0x75, 0x12, 0x4a, 0x0a, 0x0f, 0x45, 0x6e, 0x76, 0x69, 0x61, 0x72, 0x50,
+	0x72, 0x6f, 0x70, 0x75, 0x65, 0x73, 0x74, 0x61, 0x12, 0x19, 0x2e, 0x70, 0x72, 0x6f, 0x70, 0x75,
+	0x2e, 0x50, 0x72, 0x6f, 0x70, 0x75, 0x65, 0x73, 0x74, 0x61, 0x5f, 0x47, 0x65, 0x6e, 0x65, 0x72,
+	0x61, 0x64, 0x61, 0x1a, 0x1a, 0x2e, 0x70, 0x72, 0x6f, 0x70, 0x75, 0x2e, 0x52, 0x65, 0x73, 0x70,
+	0x75, 0x65, 0x73, 0x74, 0x61, 0x5f, 0x50, 0x72, 0x6f, 0x70, 0x75, 0x65, 0x73, 0x74, 0x61, 0x22,
+	0x00, 0x12, 0x45, 0x0a, 0x09, 0x56, 0x65, 0x72, 0x4c, 0x69, 0x62, 0x72, 0x6f, 0x73, 0x12, 0x1a,
+	0x2e, 0x70, 0x72, 0x6f, 0x70, 0x75, 0x2e, 0x53, 0x6f, 0x6c, 0x69, 0x63, 0x69, 0x74, 0x75, 0x64,
+	0x5f, 0x56, 0x65, 0x72, 0x4c, 0x69, 0x62, 0x72, 0x6f, 0x73, 0x1a, 0x1a, 0x2e, 0x70, 0x72, 0x6f,
+	0x70, 0x75, 0x2e, 0x52, 0x65, 0x73, 0x70, 0x75, 0x65, 0x73, 0x74, 0x61, 0x5f, 0x56, 0x65, 0x72,
+	0x4c, 0x69, 0x62, 0x72, 0x6f, 0x73, 0x22, 0x00, 0x12, 0x4e, 0x0a, 0x0e, 0x56, 0x65, 0x72, 0x55,
+	0x62, 0x69, 0x63, 0x61, 0x63, 0x69, 0x6f, 0x6e, 0x65, 0x73, 0x12, 0x1c, 0x2e, 0x70, 0x72, 0x6f,
+	0x70, 0x75, 0x2e, 0x53, 0x6f, 0x6c, 0x69, 0x63, 0x69, 0x74, 0x75, 0x64, 0x5f, 0x55, 0x62, 0x69,
+	0x63, 0x61, 0x63, 0x69, 0x6f, 0x6e, 0x65, 0x73, 0x1a, 0x1c, 0x2e, 0x70, 0x72, 0x6f, 0x70, 0x75,
+	0x2e, 0x52, 0x65, 0x73, 0x70, 0x75, 0x65, 0x73, 0x74, 0x61, 0x5f, 0x55, 0x62, 0x69, 0x63, 0x61,
+	0x63, 0x69, 0x6f, 0x6e, 0x65, 0x73, 0x22, 0x00, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -165,16 +370,24 @@ func file_propu_proto_rawDescGZIP() []byte {
 	return file_propu_proto_rawDescData
 }
 
-var file_propu_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
+var file_propu_proto_msgTypes = make([]protoimpl.MessageInfo, 6)
 var file_propu_proto_goTypes = []interface{}{
-	(*Propuesta_Generada)(nil),  // 0: propu.Propuesta_Generada
-	(*Respuesta_Propuesta)(nil), // 1: propu.Respuesta_Propuesta
+	(*Propuesta_Generada)(nil),    // 0: propu.Propuesta_Generada
+	(*Respuesta_Propuesta)(nil),   // 1: propu.Respuesta_Propuesta
+	(*Solicitud_VerLibros)(nil),   // 2: propu.Solicitud_VerLibros
+	(*Respuesta_VerLibros)(nil),   // 3: propu.Respuesta_VerLibros
+	(*Solicitud_Ubicaciones)(nil), // 4: propu.Solicitud_Ubicaciones
+	(*Respuesta_Ubicaciones)(nil), // 5: propu.Respuesta_Ubicaciones
 }
 var file_propu_proto_depIdxs = []int32{
 	0, // 0: propu.Propu.EnviarPropuesta:input_type -> propu.Propuesta_Generada
-	1, // 1: propu.Propu.EnviarPropuesta:output_type -> propu.Respuesta_Propuesta
-	1, // [1:2] is the sub-list for method output_type
-	0, // [0:1] is the sub-list for method input_type
+	2, // 1: propu.Propu.VerLibros:input_type -> propu.Solicitud_VerLibros
+	4, // 2: propu.Propu.VerUbicaciones:input_type -> propu.Solicitud_Ubicaciones
+	1, // 3: propu.Propu.EnviarPropuesta:output_type -> propu.Respuesta_Propuesta
+	3, // 4: propu.Propu.VerLibros:output_type -> propu.Respuesta_VerLibros
+	5, // 5: propu.Propu.VerUbicaciones:output_type -> propu.Respuesta_Ubicaciones
+	3, // [3:6] is the sub-list for method output_type
+	0, // [0:3] is the sub-list for method input_type
 	0, // [0:0] is the sub-list for extension type_name
 	0, // [0:0] is the sub-list for extension extendee
 	0, // [0:0] is the sub-list for field type_name
@@ -210,6 +423,54 @@ func file_propu_proto_init() {
 				return nil
 			}
 		}
+		file_propu_proto_msgTypes[2].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*Solicitud_VerLibros); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_propu_proto_msgTypes[3].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*Respuesta_VerLibros); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_propu_proto_msgTypes[4].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*Solicitud_Ubicaciones); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_propu_proto_msgTypes[5].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*Respuesta_Ubicaciones); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
@@ -217,7 +478,7 @@ func file_propu_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_propu_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   2,
+			NumMessages:   6,
 			NumExtensions: 0,
 			NumServices:   1,
 		},
@@ -244,6 +505,8 @@ const _ = grpc.SupportPackageIsVersion6
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
 type PropuClient interface {
 	EnviarPropuesta(ctx context.Context, in *Propuesta_Generada, opts ...grpc.CallOption) (*Respuesta_Propuesta, error)
+	VerLibros(ctx context.Context, in *Solicitud_VerLibros, opts ...grpc.CallOption) (*Respuesta_VerLibros, error)
+	VerUbicaciones(ctx context.Context, in *Solicitud_Ubicaciones, opts ...grpc.CallOption) (*Respuesta_Ubicaciones, error)
 }
 
 type propuClient struct {
@@ -263,9 +526,29 @@ func (c *propuClient) EnviarPropuesta(ctx context.Context, in *Propuesta_Generad
 	return out, nil
 }
 
+func (c *propuClient) VerLibros(ctx context.Context, in *Solicitud_VerLibros, opts ...grpc.CallOption) (*Respuesta_VerLibros, error) {
+	out := new(Respuesta_VerLibros)
+	err := c.cc.Invoke(ctx, "/propu.Propu/VerLibros", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *propuClient) VerUbicaciones(ctx context.Context, in *Solicitud_Ubicaciones, opts ...grpc.CallOption) (*Respuesta_Ubicaciones, error) {
+	out := new(Respuesta_Ubicaciones)
+	err := c.cc.Invoke(ctx, "/propu.Propu/VerUbicaciones", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // PropuServer is the server API for Propu service.
 type PropuServer interface {
 	EnviarPropuesta(context.Context, *Propuesta_Generada) (*Respuesta_Propuesta, error)
+	VerLibros(context.Context, *Solicitud_VerLibros) (*Respuesta_VerLibros, error)
+	VerUbicaciones(context.Context, *Solicitud_Ubicaciones) (*Respuesta_Ubicaciones, error)
 }
 
 // UnimplementedPropuServer can be embedded to have forward compatible implementations.
@@ -274,6 +557,12 @@ type UnimplementedPropuServer struct {
 
 func (*UnimplementedPropuServer) EnviarPropuesta(context.Context, *Propuesta_Generada) (*Respuesta_Propuesta, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method EnviarPropuesta not implemented")
+}
+func (*UnimplementedPropuServer) VerLibros(context.Context, *Solicitud_VerLibros) (*Respuesta_VerLibros, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method VerLibros not implemented")
+}
+func (*UnimplementedPropuServer) VerUbicaciones(context.Context, *Solicitud_Ubicaciones) (*Respuesta_Ubicaciones, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method VerUbicaciones not implemented")
 }
 
 func RegisterPropuServer(s *grpc.Server, srv PropuServer) {
@@ -298,6 +587,42 @@ func _Propu_EnviarPropuesta_Handler(srv interface{}, ctx context.Context, dec fu
 	return interceptor(ctx, in, info, handler)
 }
 
+func _Propu_VerLibros_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(Solicitud_VerLibros)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(PropuServer).VerLibros(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/propu.Propu/VerLibros",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(PropuServer).VerLibros(ctx, req.(*Solicitud_VerLibros))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _Propu_VerUbicaciones_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(Solicitud_Ubicaciones)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(PropuServer).VerUbicaciones(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/propu.Propu/VerUbicaciones",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(PropuServer).VerUbicaciones(ctx, req.(*Solicitud_Ubicaciones))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 var _Propu_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "propu.Propu",
 	HandlerType: (*PropuServer)(nil),
@@ -305,6 +630,14 @@ var _Propu_serviceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "EnviarPropuesta",
 			Handler:    _Propu_EnviarPropuesta_Handler,
+		},
+		{
+			MethodName: "VerLibros",
+			Handler:    _Propu_VerLibros_Handler,
+		},
+		{
+			MethodName: "VerUbicaciones",
+			Handler:    _Propu_VerUbicaciones_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
